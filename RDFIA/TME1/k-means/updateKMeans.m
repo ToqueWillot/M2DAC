63b7caac006de999b6126d953831b0a912f4cea0
floriantoque <florian.toque@gmail.com>
2015-10-05 19:04:44 +0200

    [FDMS] time first part

--- RDFIA/TME1/k-means/updateKMeans.m
+++ RDFIA/TME1/k-means/updateKMeans.m
@@ -1,7 +1,20 @@
-function [ newcenters , errorq , movecenters ] = updateKMeans( listPts , centers  , nc)
-
-nc
-	newcenters = 0;
-	errorq=0;
-      movecenters=0;
+function [ newcenters , erreur , movecenters ] = updateKMeans( listPts , centers  , nc)
+	newcenters=centers;%??
+    movecenters=0;
+    erreur=0;
+    for c=1:size(centers,1)
+        moyenne = mean(listPts(nc==c,:));
+        if(moyenne>0)
+            for d=1:size(centers,2)
+                newcenters(c,d) = moyenne(1,d);
+            end
+        else
+           newcenters(c,:)=zeros(1,size(centers,2));
+        end
+size(listPts(nc==c,:))
+size(newcenters(c,:))
+        erreur=erreur+sum(sum(power(listPts(nc==c,:)-repelem(newcenters(c,:),size(listPts(nc==c,:),1),1),2)),2);
+     end
+    erreur
+    movecenters=sum(sum(power(centers-newcenters,2),1),2);
 end
