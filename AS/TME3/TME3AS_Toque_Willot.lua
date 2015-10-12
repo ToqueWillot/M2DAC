6ad442d51f79b80af33276e440047dd4d4186317
floriantoque <florian.toque@gmail.com>
2015-10-12 03:23:30 +0200

    [AS] tme 3 fini et send

--- AS/TME3/TME3AS_Toque_Willot.lua
+++ AS/TME3/TME3AS_Toque_Willot.lua
@@ -34,24 +34,24 @@ function create_multiclass_xy(params)
 end
 
 
---__________test gaussian generation_________________
--- local n=3; local position={3,3}
--- local x_test = create_x(n,position);
--- print(x_test)
+-- --__________test gaussian generation_________________
+-- -- local n=3; local position={3,3}
+-- -- local x_test = create_x(n,position);
+-- -- print(x_test)
+-- --
+-- -- local n=3; local position={3,3}; label=4
+-- -- local x,y = create_x_y(n,position,label);
+-- -- print(x)
+-- -- print(y)
+-- --
+-- -- local params = {{3,{3,3},4},{3,{-3,-3},2}}
+-- -- local x,y = create_multiclass_xy (params)
+-- -- print(x)
+-- -- print(y)
 --
--- local n=3; local position={3,3}; label=4
--- local x,y = create_x_y(n,position,label);
--- print(x)
--- print(y)
 --
--- local params = {{3,{3,3},4},{3,{-3,-3},2}}
--- local x,y = create_multiclass_xy (params)
--- print(x)
--- print(y)
-
-
-
---Plot the world___________________________________
+--
+-- --Plot the world___________________________________
 
 function toPlot2D(x,label,color)
    return {label, x[{{},1}], x[{{},2}], 'p ls 1 lc rgb "'..color..'"'}
@@ -102,6 +102,23 @@ function getGridtoPlot(model,x,labels,colorsGrid)
   return toPlot
 end
 
+function getGridtoPlot3couches(couche1,couche2,couche3,x,labels,colorsGrid)
+  toPlot = {}
+  xGrid = getPointsGrid(x,100)
+  xGridInputs = xGrid
+  local yGrid11 = couche1:forward(xGrid)
+  local yGrid22 = couche2:forward(yGrid11)
+  local yGrid33 = couche3:forward(yGrid22)
+  local yGrid = yGrid33:sign()
+
+  indices = torch.linspace(1,yGrid:size(1),yGrid:size(1)):long()
+  for i = 1, #labels do
+     local selected = indices[yGrid:eq(labels[i])]
+     table.insert(toPlot, toPlot2D(xGrid:index(1, selected), "grid "..labels[i], colorsGrid[i]))
+  end
+  return toPlot
+end
+
 
 function getPointstoPlot(x,y,labels,colors)
   toPlot = {}
@@ -115,6 +132,17 @@ end
 
 function plot_decision(x,y,model,labels,colors,colorsGrid,name)
   local toPlot = getGridtoPlot(model,x,labels,colorsGrid)
+  local toPlot2 = getPointstoPlot(x,y,labels,colors)
+  for i=1,#toPlot2 do
+    table.insert(toPlot, toPlot2[i])
+  end
+  gnuplot.pngfigure(name)
+  gnuplot.plot(toPlot)
+  gnuplot.close()
+end
+
+function plot_decision3couches(x,y,couche1,couche2,couche3,labels,colors,colorsGrid,name)
+  local toPlot = getGridtoPlot3couches(couche1,couche2,couche3,x,labels,colorsGrid)
   local toPlot2 = getPointstoPlot(x,y,labels,colors)
   for i=1,#toPlot2 do
     table.insert(toPlot, toPlot2[i])
@@ -150,92 +178,153 @@ function plot_decision3dim(x,y,model,labels,colors,colorsGrid,name)
   gnuplot.close()
 end
 
---test plot______________________
-local params = {{10,{3,3},1},{10,{-3,-3},-1}}
-local xxx,yyy = create_multiclass_xy (params)
-labels={1,-1}
-colors={"red","blue"}
-name="test points.png"
+-- --test plot______________________
+-- local params = {{10,{3,3},1},{10,{-3,-3},-1}}
+-- local xxx,yyy = create_multiclass_xy (params)
+-- labels={1,-1}
+-- colors={"red","blue"}
+-- name="test points.png"
+--
+-- -- print(xxx)
+-- -- print(yyy)
+-- -- plot_points(xxx,yyy,labels,colors,name)
+--
+-- -- gridX=getRegionGrid(xxx,80)
+-- -- y = torch.ones(gridX:size(1))
+-- -- print("taillegridX"..gridX:size(1))
+-- -- plot_points(gridX,y,{1},{"red"},"gridred.png")
+--
 
--- print(xxx)
--- print(yyy)
--- plot_points(xxx,yyy,labels,colors,name)
 
--- gridX=getRegionGrid(xxx,80)
--- y = torch.ones(gridX:size(1))
--- print("taillegridX"..gridX:size(1))
--- plot_points(gridX,y,{1},{"red"},"gridred.png")
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------Commencement du TME 3 ------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+--Binome Florian Toqué et Paul Willot
+--Master 2 DAC , UE AS (apprentissage statistique)
+--Professeur Patrick Gallinari et Ludovic Denoyer
 
+--TME3
+--Exercice 1 implémentation de la fonction d'activation Requ et d'un model linéaire !!! :)
 
+--1.1 implémentation de la fonction d'activation Requ
 
+-- la fonction requ n'a pas de parametre on ne met donc pas a jour ses poids. (sa derivée en fonction de ses parametres est nulle)
 
---fit the model with x and y
-function fit_model(x,y,model,nIter,ep,criterion,layer)
-  for i = 1,nIter do
-     model:zeroGradParameters()
-     y_chap = model:forward(x)
-     loss = criterion:forward(y_chap, y)
-     grad = criterion:backward(y_chap, y)
-     model:backward(x, grad)
-     model:updateParameters(ep)
-  end
-  return model
+local ReQu, parent = torch.class('nn.ReQu', 'nn.Module')
+
+function ReQu:__init()
+   parent.__init(self)
+end
+
+function ReQu:updateOutput(input)
+   self.output = torch.pow(input,2)
+   self.inputIsPositive = input:ge(0):double() -- condition if de la fonction requ ( => probleme classif xor en non lineaire)
+   self.output:cmul(self.inputIsPositive)
+   return self.output
+end
+
+function ReQu:updateGradInput(input, gradOutput)
+   -- On veut le gradient du Loss en fonction de notre entrée (input).
+   -- grad input egal gradoutput * grad de la fonction d'erreur en fonction des x (inputs)
+   self.gradInput = input * 2
+   self.gradInput:cmul(self.inputIsPositive)
+   -- Ci dessous, gradOutput est le gradient du loss en fonction des
+   -- entrées du module suivant (ou sorties du module actuel)
+   self.gradInput:cmul(gradOutput)
+   return self.gradInput
 end
 
 
 
+--1.2 implémentation de la fonction Lineaire
+-- la fonction requ n'a pas de parametre on ne met donc pas a jour ses poids. (sa derivée en fonction de ses parametres est nulle)
 
+local Lineaire, Parent = torch.class('nn.Lineaire', 'nn.Module')
+
+function Lineaire:__init(inputSize, outputSize)
+   Parent.__init(self)
+   self.weight = torch.Tensor(outputSize, inputSize)
+   self.gradWeight = torch.Tensor(outputSize, inputSize)
+   self:reset()
+end
+
+--fonction utilisée par le forward on calcul l'output
+function Lineaire:updateOutput(x)
+   if x:dim() == 1 then
+       self.output = self.weight * x -- si entrée = vecteur
+   else
+       self.output = x * self.weight:t() -- si entrée = matrice
+   end
+   return self.output
+end
+
+function Lineaire:updateGradInput(x, gradOutput)
+    -- Le gradient des sorties dune fonction lineaire en fonction des entrées est égal aux paramètres (theta*x)'/x'=theta d'où
+   self.gradInput = self.weight:transpose(1,2) * gradOutput
+   return self.gradInput
+end
+
+--mise à jour des parametres
+-- multiplication de gradoutput par (x :qui est la derivée de la fonction lineaire en fonction des parametres)
+function Lineaire:accGradParameters(x, gradOutput)
+   self.gradWeight = self.gradWeight + gradOutput:reshape(gradOutput:size(1),1) * x:reshape(1,x:size(1))
+   return self.gradWeight
+end
+
+    -- Initialisation des paramètres entre -1 et 1
+function Lineaire:reset()
+    self.weight = self.weight:uniform() * 2 - 1
+end
+
+
+
+-- 1.3 test sur des données jouets de la fonction lineaire
 
 -- ______classif_linear_2gauss
--- exemple of classification with linear model. criterion = MSE (mean squared error)
+-- exemple of classification with lineaire. criterion = MSE (mean squared error) donées jouets
 local params = {{20,{3,3},1},{20,{-3,-3},-1}}
 local x,y = create_multiclass_xy (params) -- create data x (gaussian) and y (labels)
 labels={1,-1}
-colors={"red","blue"}
-colorsGrid={"pink","cyan"}
-name="classif_linear_2gauss.png"
+colors={"blue","yellow"}
+colorsGrid={"grey","pink"}
+name="classif_lineaire_2gauss.png"
 
-nIter=5000
-ep=0.0005
+nIter=1000
+ep=0.00001
 criterion=nn.MSECriterion()
 layer={2,1} --2 inputs (x[1], x[2]) one output (label)
 
-model = nn.Linear(layer[1],layer[2])
-modelfit = fit_model(x,y,model,nIter,ep,criterion,layer)
+model = nn.Lineaire(layer[1],layer[2])
+for i = 1, nIter do
+   shuffle = torch.randperm(x:size(1))
+   for j = 1, x:size(1) do
+      id = shuffle[j]
+      input = x[id]
+      label = torch.Tensor{y[id]}
+      model:zeroGradParameters()
+      output = model:forward(input)
+      loss = criterion:forward(output, label)
+      df_doutput = criterion:backward(output, label)
+      df_dinput = model:backward(input, df_doutput)
+      model:updateParameters(ep)
+   end
+end
+-- print(x)
+-- print(y)
+-- print(model:forward(x):sign())
+plot_decision(x,y,model,labels,colors,colorsGrid,name)
 
-plot_decision(x,y,modelfit,labels,colors,colorsGrid,name)
-
-
-
-
---______xor model lineaire
--- le model lineaire ne permet pas de séparer l'espace en trois
-local params = {{20,{3,3},1},
-                {20,{-3,-3},1},
-                {20,{-3,3},-1},
-                {20,{3,-3},-1}}
-local x,y = create_multiclass_xy (params)
-labels={1,-1}
-colors={"red","blue"}
-colorsGrid={"pink","cyan"}
-name="classif_linear_XOR.png"
-
-nIter=10000
-ep=0.0001
-criterion=nn.MSECriterion()
-layer={2,1}
-model = nn.Linear(layer[1],layer[2])
-modelfit = fit_model(x,y,model,nIter,ep,criterion,layer)
-
-plot_decision(x,y,modelfit,labels,colors,colorsGrid,name)
+--la fonction lineaire separe classifie sans soucis les données jouets.
 
 
 
 
-
-
-
---_____xor kernel tricks___________________________
+--exercice 2 xor kernel tricks___________________________
 local params = {{20,{3,3},1},
                 {20,{-3,-3},1},
                 {20,{-3,3},-1},
@@ -250,111 +339,94 @@ il faut rajouter une dimension x[1]*x[2] ainsi meme le modele lineaire peut clas
 local new_x= torch.cat(x, torch.cmul(x[{{},1}],x[{{},2}]) ,2)
 
 labels={1,-1}
-colors={"red","blue"}
-colorsGrid={"pink","cyan"}
-name="classif_linear_XOR_KernelTrick.png"
+colors={"blue","yellow"}
+colorsGrid={"grey","pink"}
+name="classif_lineaire_XOR_kerneltrick.png"
 
-nIter=50000
+nIter=1000
 ep=0.00005
 criterion=nn.MSECriterion()
 layer={3,1} --3 inputs (x[1], x[2], x[1]*x[2]) one output (label)
-model = nn.Linear(layer[1],layer[2])
-modelfit = fit_model(new_x,y,model,nIter,ep,criterion,layer2)
-plot_decision3dim(x,y,modelfit,labels,colors,colorsGrid,name)
+model = nn.Lineaire(layer[1],layer[2])
+
+for i = 1, nIter do
+   shuffle = torch.randperm(new_x:size(1))
+   for j = 1, new_x:size(1) do
+      id = shuffle[j]
+      input = new_x[id]
+      label = torch.Tensor{y[id]}
+      model:zeroGradParameters()
+      output = model:forward(input)
+      loss = criterion:forward(output, label)
+      df_doutput = criterion:backward(output, label)
+      df_dinput = model:backward(input, df_doutput)
+      model:updateParameters(ep)
+   end
+end
+-- print(new_x)
+-- print(y)
+-- print(model:forward(new_x):sign())
+
+
+plot_decision3dim(new_x,y,model,labels,colors,colorsGrid,name)
+--A laide du kernel trick la fonction linéaire arrive sans soucis à classifié lensemble des points
 
 
 
---_____xor reseau de neuronnes 3 couches___________________________
+-- 1.3 Résolution du XOR sans kerneltrick( reseau de neuronnes à trois couche lineaire, requ, lineaire)
+
+-- ______data xor
 local params = {{20,{3,3},1},
                 {20,{-3,-3},1},
                 {20,{-3,3},-1},
                 {20,{3,-3},-1}}
 local x,y = create_multiclass_xy (params)
 
-
---[[A laide d'un reseau de neuronnes à trois couches il est possible de classifier
-les données "XOR" . la couche cachée représente la fonction tanh (permettant de
-découpé l'espace en elipse)]]--
-
 labels={1,-1}
-colors={"red","blue"}
-colorsGrid={"pink","cyan"}
-name="classif_linear_XOR_NN3Layer.png"
+colors={"blue","yellow"}
+colorsGrid={"grey","pink"}
+name="classif_XOR_nonLineaire.png"
 
-nIter=50000
-ep=0.0005
+nIter=2000
+ep=0.00001
 criterion=nn.MSECriterion()
-model=nn.Sequential()
-model:add(nn.Linear(2,3))
-model:add(nn.Tanh(true))
-model:add(nn.Linear(3,1))
-modelfit = fit_model(x,y,model,nIter,ep,criterion,layer2)
-plot_decision(x,y,modelfit,labels,colors,colorsGrid,name)
+layer1={2,3} --2 inputs (x[1], x[2]) one output (label)
+layer3={3,1}
 
+local couche1 = nn.Lineaire(layer1[1],layer1[2])
+local couche2 = nn.ReQu()
+local couche3 = nn.Lineaire(layer3[1],layer3[2])
+for i = 1, nIter do
+   shuffle = torch.randperm(x:size(1))
+   for j = 1, x:size(1) do
+      id = shuffle[j]
+      input = x[id]
+      label = torch.Tensor{y[id]}
+      couche1:zeroGradParameters()
+      couche2:zeroGradParameters()
+      --forward pour chaque couche
+      output1 = couche1:forward(input)
+      output2 = couche2:forward(output1)
+      output3 = couche3:forward(output2)
+      --loss et backward du criterion pour avoir derive du loss en fonction de son output
+      loss = criterion:forward(output3, label)
+      df_doutput3 = criterion:backward(output3, label)
 
---[[
- -- Notes de deepsky:
-g = nn.Linear()
-t = nn.Tanh()
-h = nn.Linear()
-c = nn.MSECriterion()
+      --propagation du gradient de l'erreur sur les couches précédentes
+      df_dinput3 = couche3:backward(output2, df_doutput3)
+      df_dinput2 = couche2:backward(output1, df_dinput3)
+      df_dinput1 = couche1:backward(input, df_dinput2)
 
-out_1 = g:forward(x)
-out_2 = t:forward(out_1)
-out_3 = h:forward(out_2)
-loss = c:forward(out_3,y)
-delta1 = c:backward(out_3,y)
-delta2 = h:updateParameters(delta1) -- delta1 ou autre chose
-
-]]--
-
-Requ, Parent = torch.class('Requ', 'nn.Module')
-
-function Requ:__init()
-   parent.__init(self)
+      couche1:updateParameters(ep)
+      couche3:updateParameters(ep)
+   end
 end
 
-function Requ:updateOutput(x)
-  local t = torch.Tensor(x:size(1))
-  --[[
-  for i=1,x:size(1) do
-    if x[i]>0 do
-      t[i] = x[i]*x[i]
-    else
-      t[i] = 0
-    end
-  end
-  ]]
-  -- t:clamp(0,999999)
-  clamp = t:gt(0)
-  x = torch.cmul(x[{{}}],clamp)
-  t = torch.cmul(x[{{}}],x[{{}}])
-  self.output = t
-  return self.output
-  -- torch.cmul(x[{{},1}],x[{{},2}])
-  --1/(1+nn.math.exp(-x))
-end
+--
+-- print(x)
+-- print(y)
+plot_decision3couches(x,y,couche1,couche2,couche3,labels,colors,colorsGrid,name)
 
-function Requ:updateGradInput(input, gradOutput)
-  local t = torh.Tensor(input:size(1))
-  for i=1,input:size(1) do
-    t[i] = gradOutput[i]*input[i]
-  end
-  self.gradInput = t
-  return self.gradInput
-end
-
-function Requ:accGradParameters(input, gradOutput)
-end
- 
-function Requ:reset()
-end
-
-
-yGrid:eq(labels[i])
-yGrid:eq(labels[i])
-tt = torch.Tensor({2,0,-1})
-tt = {2,0,-1}
-tt
-tt:gt(0)
-print({2,0,-1})
+--on remarque ici que l'une des parties est mal classifié ceci est du au fait que la fonction requ mette à 0 les valeurs negatives.
+-- Lors de la classification une partie des points obtient le label 0 qui n'existe pas. la back propagation ne permet pas
+--de mettre a jour les poids etant donné que la derivée de la fonction d'erreur en fonction des poids est nulle.
