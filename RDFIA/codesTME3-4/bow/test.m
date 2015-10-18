fc0692d07fd06818f5887ccbfc82bda71b99ca06
floriantoque <florian.toque@gmail.com>
2015-10-18 16:49:34 +0200

    [RDFIA] rapport v0.1

--- RDFIA/codesTME3-4/bow/test.m
+++ RDFIA/codesTME3-4/bow/test.m
@@ -1,4 +1,14 @@
-sifts=1
-clusters=2
-matNormClusters=2
-[ bow,nc ] = computeBow( sifts, clusters , matNormClusters)
+sifts=1;
+clusters=2;
+matNormClusters=2;
+[ bow,nc ] = computeBow( sifts, clusters , matNormClusters);
+
+sifts=[1,2,3;6,6,6;9,9,9];
+clusters=[5,5,5,2;8,8,8,2;1,2,2,2];
+squareNE=0;
+
+[nc] = assignementKMeans(sifts,clusters,squareNE);
+
+sifts.' .* 
+
+size(clusters.')
